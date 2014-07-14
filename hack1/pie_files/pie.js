70533dad11befe825515e349a118431caf0072b6
sutt <wsutton17@gmail.com>
2014-07-14 19:13:53 -0400

    progress on updating wheel values

--- hack1/pie_files/pie.js
+++ hack1/pie_files/pie.js
@@ -3,15 +3,9 @@ var width = 500,
 		radius = Math.min(width, height) / 2;
 
 	var color = d3.scale.category20();
-var arr = ['#ffffff',
-'#f0f0f0',
-'#d9d9d9',
-'#bdbdbd',
-'#969696',
-'#737373',
-'#525252',
-'#252525',
-'#000000'];
+
+/* https://github.com/mbostock/d3/blob/master/lib/colorbrewer/colorbrewer.js */
+var arr = ["#f7fbff","#deebf7","#c6dbef","#9ecae1","#6baed6","#4292c6","#2171b5","#08519c","#08306b"];
 	//var arr = ['rgb(247,252,253)','rgb(229,245,249)','rgb(204,236,230)','rgb(153,216,201)','rgb(102,194,164)','rgb(65,174,118)','rgb(35,139,69)','rgb(0,109,44)','rgb(0,68,27)'];
 	//var arr =['rgb(247,252,253)','rgb(224,236,244)','rgb(191,211,230)','rgb(158,188,218)','rgb(140,150,198)','rgb(140,107,177)','rgb(136,65,157)','rgb(129,15,124)','rgb(77,0,75)'];
 	//var arr =['rgb(255,255,255)','rgb(240,240,240)','rgb(217,217,217)','rgb(189,189,189)','rgb(150,150,150)','rgb(115,115,115)','rgb(82,82,82)','rgb(37,37,37)','rgb(0,0,0)'];
@@ -25,6 +19,7 @@ var arr = ['#ffffff',
 		.value(function(d) { return d; })
 		.sort(null);
 	
+	
 	var pie2 = d3.layout.pie()
 		//.value(function(d) { return d.apples; })
 		.value(function(d) { return d; })
@@ -34,7 +29,7 @@ var arr = ['#ffffff',
 		.innerRadius(radius - 60)
 		.outerRadius(radius - 20);
 
-	var arc2 = d3.svg.arc()
+	var arctwo = d3.svg.arc()
 		.innerRadius(radius - 100)
 		.outerRadius(radius - 60);
 		
@@ -92,6 +87,8 @@ var arr = ['#ffffff',
 	  }
 	});
 	*/
+	
+	
 	//d3.json("jsonStart.json", function(error, data) {
 	d3.json("ggboth.json", function(error, data) {
 		
@@ -99,8 +96,8 @@ var arr = ['#ffffff',
 		console.log(data);
 		myDebug = data;
 		
-		// d3.selectAll("#apples_input")
-		  // .on("change", change);
+		d3.selectAll("#rotate_input")
+		   .on("click", rotate_outer);
 		  
 		var myStr = 'outsideGenes' ;
 		single_data = _.filter(data, function(x) { 
@@ -108,6 +105,7 @@ var arr = ['#ffffff',
 		
 		my_data = single_data.map( function(x) {return x.geneLen;});
 		myDebug = my_data;
+	
 		var path = svg.datum(my_data).selectAll("path")
 		  .data(pie)
 		.enter().append("path")
@@ -120,29 +118,80 @@ var arr = ['#ffffff',
 										return x[myStr]; } )[0][myStr];
 		my_data2 = single_data2.map( function(x) {return x.geneLen;});
 		myDebug2 = my_data2;
-		//var path = svg2.datum(my_data2['geneLen']).selectAll("path")
-		var path = svg2.datum(my_data2).selectAll("path")
+		
+		var path2 = svg2.datum(my_data2).selectAll("path")
 		  .data(pie2)
 		.enter().append("path")
 		  .attr("fill", function(d, i) {  return my_color( i )})
-		  .attr("d", arc2)
+		  .attr("d", arctwo)
 		  .each(function(d) { this._current = d; }); // store the initial angles
+	
+	
+	
+	function rotate_outer() {
+		
+		console.log('in rotate');
+		//pie.startAngle(2);
+		//pie.endAngle(function() { return 4 }); //(Math.PI * 2) + 2});
+		//pie.value(function(d) { return d + (10000); }); // change the value function
+		//pie.value(function(d) { return d + (10000); }); // change the value function
+		//svg.datum(myDebug).selectAll('path')
+		
+		newpath = svg.datum(myDebug2).selectAll('path')
+			.data(pie)
+			//.enter() //.append('path')
+			.attr("d",arc);
+		
+		newpath.transition().duration(750).attrTween("d",arcTween);
+			//newpath.transition().duration(750).styleTween("fill",fillTween);
+		
+			//.attr('fill','#000000');
+		//pie.value(function(d) { return d * 10; }); // change the value function
+		//path = path.data(pie); // compute the new angles
+		//path.transition().duration(750).attrTween("d", arcTween); // redraw the a
+		/*
+		path3 = svg.selectAll("path")
+		  .data(pie)
+		.enter().append("path")
+		  .attr("fill", function(d, i) {  return '#FFFFF'})
+		  .attr("d", arc)
+		  .each(function(d) { this._current = d; });
+		  
+		path3.transition().style("fill", '#000000');
+		*/
+	 	
+	/*
+		path = svg.datum(myDebug2).selectAll("path")
+		  .data(pie)
+		.enter().append("path")
+		  .attr("fill", function(d, i) {  return my_color( i )})
+		  .attr("d", arc)
+		  .each(function(d) { this._current = d; });
+	*/	
+	
+	}
+		
 	});
 	
-	
-	function type(d) {
-	  d.apples = +d.apples;
-	  d.oranges = +d.oranges;
-	  return d;
-	}
-
 	// Store the displayed angles in _current.
 	// Then, interpolate from _current to the new angles.
 	// During the transition, _current is updated in-place by d3.interpolate.
 	function arcTween(a) {
 	  var i = d3.interpolate(this._current, a);
+	  console.log(a);
 	  this._current = i(0);
 	  return function(t) {
+		console.log(t);
 		return arc(i(t));
+	  };
+	}
+
+	function fillTween(a) {
+	  //var i = d3.interpolate(this._current, a);
+	  console.log(this.style('fill'));
+	  //this._current = i(0);
+	  return function(t) {
+		
+		return arr[Math.round(t*7)];
 	  };
 	}
